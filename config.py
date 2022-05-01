202042786940a0029be15e2d8015e2a6516203c9
kschoi <kyungsik0907@gmail.com>
2022-05-01 10:21:55 +0900

    Add database + config

---
+++ config.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+import os
+
+# .env 파일 auto load
+load_dotenv()
+
+
+class Config(object):
+    TESTING = False
+    DEBUG = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = f"mysql://{os.getenv('DB_PRODUCT_USER')}:" \
+                              f"{os.getenv('DB_PRODUCT_PWD')}@" \
+                              f"{os.getenv('DB_PRODUCT_HOST')}:" \
+                              f"{os.getenv('DB_PRODUCT_PORT')}/" \
+                              f"{os.getenv('DB_PRODUCT_NAME')}?charset=utf8"
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = f"mysql://{os.getenv('DB_USER')}:" \
+                              f"{os.getenv('DB_PWD')}@" \
+                              f"{os.getenv('DB_HOST')}:" \
+                              f"{os.getenv('DB_PORT')}/" \
+                              f"{os.getenv('DB_NAME')}?charset=utf8"
+
+
+class TestingConfig(Config):
+    TESTING = True
--- toy/__init__.py
+++ toy/__init__.py
@@ -1,8 +1,28 @@
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
+
+    # config
+    config = app.config.get('ENV')
+    if config == 'production':
+        app.config.from_object('config.ProductionConfig')
+    elif config == 'testing':
+        app.config.from_object('config.TestingConfig')
+    else:
+        app.config.from_object('config.DevelopmentConfig')
+
+    # database
+    from toy.models import example_models
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # routes list
     from .routes import routes_list
